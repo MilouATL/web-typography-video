4d311c441f67bef1187804c5df2015bd804217a8
miloudeveen <miloudeveen@gmail.com>
2019-05-22 21:35:35 +0200

    initial commit
    added all the assets, responsive video and added animations

---
+++ sounds.js
@@ -0,0 +1,17 @@
+// Hier kan je tijden toevoegen in secondes
+// Op die mommenten komt er een class op de body.
+sounds = [
+	9,
+	22.2,
+	30.5,
+	43.2, 
+	49,
+	50.5,
+	51.2,
+	56,
+	57.2, 
+	63.5,
+	78, 
+	90,
+	91.5
+];
